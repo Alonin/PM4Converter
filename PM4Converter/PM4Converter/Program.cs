7572c2195488b13cc07c7363547a132629802d1e
Tim-Couch <timothy.couch2@gmail.com>
2022-09-06 13:55:07 -0500

    parse msvt

--- PM4Converter/PM4Converter/Program.cs
+++ PM4Converter/PM4Converter/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+
 Console.WriteLine("Please enter the path to your PM4 & ADT Files.");
 var folderpath = Console.ReadLine();
 string[] PM4Files = Directory.GetFiles(folderpath, "*.pm4");
@@ -9,16 +11,43 @@ foreach (var pm4file in PM4Files)
     using (BinaryReader PM4BinaryReader = new BinaryReader(PM4Stream))
     using (BinaryWriter PM4BinaryWriter = new BinaryWriter(PM4Stream))
     {
-        var token = PM4BinaryReader.ReadUInt32();
-        var pos = PM4BinaryReader.BaseStream.Position;
-        var size = (9 * 9 + 8 * 8);
-        if(token == 1296258644) // mcvt
+        while (PM4BinaryReader.BaseStream.Position != PM4BinaryReader.BaseStream.Length)
         {
-            while (PM4BinaryReader.BaseStream.Position < pos + size)
+
+
+            var token = PM4BinaryReader.ReadUInt32();
+            var size = PM4BinaryReader.ReadUInt32();
+            var pos = PM4BinaryReader.BaseStream.Position;
+            var i = 0;
+            // Console.WriteLine("in MSVT.");
+            while (token == 1297307220) // MSVT
             {
-                Console.WriteLine(size);
+                Console.WriteLine("MSVT Found.");
+                while (PM4BinaryReader.BaseStream.Position < pos + size)
+                { 
+
+                            
+                            
+                            var posx = PM4BinaryReader.ReadSingle();
+                            var posy = PM4BinaryReader.ReadSingle();
+                            var height = PM4BinaryReader.ReadSingle();
+                            Console.WriteLine(posx + " " + posy + " " + height + " " + i );
+                    i++;
+                    
+                  
+                   
                     }
+
+                break;
+            }
+
+          
+                Console.WriteLine("OUT OF MSVT!!");
+            //    PM4BinaryReader.BaseStream.Position = 0;
+            PM4BinaryReader.BaseStream.Position = pos + size;
         }
+    
+    }
        
     }
-}
+    
